df075be5a7a2e1ff19b42f6d64ea8e0a6815f3ef
Yug Upadhyay <114340240+YUG2121@users.noreply.github.com>
2025-08-31 20:14:47 +0530

    Update vite.config.js

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
   plugins: [react(),
         tailwindcss(),
   ],
-  server:{port:5174}
+  server:{port:5174},
+  optimizeDeps: {
+    include: ['react/jsx-runtime']
+  }
 })
